b03d559f2872fb44ab15f2c3812dd5a397900123
召 程 <542153354@qq.com>
2019-10-12 10:25:28 +0800

    添加项目文件。

---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AdapterPattern
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var turkey = new WildTurkey();
+            var adapter = new TurkeyAdapter(turkey);
+
+            Tester(adapter);
+        }
+
+        private static void Tester(IDuck duck)
+        {
+            duck.Fly();
+            duck.Quack();
+        }
+    }
+}
